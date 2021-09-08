7c3d75a11b20bc4b5d74cb558b7a2a5cf9442720
Tajveez Rehman <tajveez786@gmail.com>
2021-09-08 15:25:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
-const draggables = document.querySelectorAll(".draggable");
-const containers = document.querySelectorAll(".container");
+const draggables = document.querySelectorAll('.draggable')
+const dragPoints = document.querySelectorAll('.dragPoint')
+const containers = document.querySelectorAll('.container')
+dragPoints.forEach(dragPoint => {
+  dragPoint.addEventListener('mouseover', () => dragPoint.parentElement.draggable = true)
+  dragPoint.addEventListener('mouseout', () => dragPoint.parentElement.draggable = false)
+})
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
 
-draggables.forEach((draggable) => {
-  draggable.addEventListener("dragstart", () => {
-    console.log("drag Start");
-  });
-});
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
